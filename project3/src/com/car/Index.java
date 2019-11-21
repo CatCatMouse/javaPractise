4d3d58ed4ccee21b2f89c642b1540f660667f098
caiwei <980440825@qq.com>
2019-11-21 23:24:25 +0800

    change something at home

--- project3/src/com/car/Index.java
+++ project3/src/com/car/Index.java
@@ -18,27 +18,25 @@ public class Index {
 			carsNumber = input.nextInt();
 		}while(carsNumber <= 0);
 		
-		while (carsNumber > 0)
-		{
-			int num = 0;
-			while (num != 0) {
-				System.out.println("您可租车的类型及其价目表:");
-				System.out.println("序号	汽车名称	租金		容量");
-				System.out.println("1.	奥迪A4	500元/天		载人:4人");
-				System.out.println("2.	马自达6	400元/天		载人:4人");
-				System.out.println("3.	皮卡雪6	450元/天		载人:4人 载货:2吨");
-				System.out.println("4.	金龙	800元/天		载人:20人");
-				System.out.println("5.	松花江	400元/天		载货:4吨");
-				System.out.println("6.	依维柯	1000元/天		载货:20吨");
-				num = input.nextInt();
-				
-				if (num <=0 || num > 6)
-				{
-					System.out.println("请输入正确的租出序号");
-				}
-			}
-		}
+		System.out.println("您可租车的类型及其价目表:");
+		System.out.println("序号	汽车名称	租金		容量");
+		System.out.println("1.	奥迪A4	500元/天		载人:4人");
+		System.out.println("2.	马自达6	400元/天		载人:4人");
+		System.out.println("3.	皮卡雪6	450元/天		载人:4人 载货:2吨");
+		System.out.println("4.	金龙	800元/天		载人:20人");
+		System.out.println("5.	松花江	400元/天		载货:4吨");
+		System.out.println("6.	依维柯	1000元/天		载货:20吨");
 		
+		//选择的车序号合集
+		int[] cars = new int[carsNumber];
+		
+		
+		for (int i=1; i<=carsNumber; i++) {
+			System.out.println("请输入第"+i+"辆车的序号:");
+			int num = input.nextInt();
+			ACar car = new Car(num);
+			
+		}
 		
 		
 		System.out.println("");
